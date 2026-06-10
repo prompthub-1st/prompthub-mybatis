5b0750df0841c8ae743b62f777d8880573de232d
ironhamlab <03110315@naver.com>
2026-06-10 17:50:20 +0900

    feat: update dto,dao

--- src/main/java/com/prompthub/prompt/model/dao/PromptMapper.java
+++ src/main/java/com/prompthub/prompt/model/dao/PromptMapper.java
@@ -1,4 +1,23 @@
 package com.prompthub.prompt.model.dao;
 
-public class PromptMapper {
+import com.prompthub.prompt.model.dto.PromptDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface PromptMapper {
+
+    List<PromptDTO> selectAllPrompts();
+
+    PromptDTO selectPromptById(@Param("promptId") Long promptId);
+
+    int insertPrompt(PromptDTO prompt);
+
+    int updatePrompt(PromptDTO prompt);
+
+    int deletePrompt(@Param("promptId") Long promptId);
+
+    int increaseViewCount(@Param("promptId") Long promptId);
 }
