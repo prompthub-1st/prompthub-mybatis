8c586c8bcc57e8e2eb35d7ecdd2c5a39ae4c8444
dmstj0162 <dmstj0162@gmail.com>
2026-06-11 16:08:09 +0900

    feat : add user login api with mybatis

--- src/main/java/com/prompthub/Application.java
+++ src/main/java/com/prompthub/Application.java
@@ -1,5 +1,5 @@
 
-package com.prompthub.configuration;
+package com.prompthub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/prompthub/user/controller/UserController.java
+++ src/main/java/com/prompthub/user/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.prompthub.user.controller;
 
+import com.prompthub.user.model.dto.LoginRequest;
+import com.prompthub.user.model.dto.UserDTO;
+import com.prompthub.user.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public UserDTO login(@RequestBody LoginRequest request) {
+        return userService.login(request.getLoginId(), request.getPassword());
+    }
 }
